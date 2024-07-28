58ee7f71a85af288c1845401d0ce7c16e20912e5
BotSmilee <94220501+BotSmilee@users.noreply.github.com>
2024-07-28 16:15:11 +0500

    Add files via upload

---
+++ Hippodrome/Hippodrome.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    static Hippodrome game;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Horse> horsesMain = new ArrayList<>();
+        horsesMain.add(new Horse("Jan", 3, 0));
+        horsesMain.add(new Horse("Amigo", 3, 0));
+        horsesMain.add(new Horse("Dio", 3, 0));
+        game = new Hippodrome(horsesMain);
+        game.run();
+        game.printWinner();
+    }
+
+    public Horse getWinner() {
+        Horse fastest = horses.get(0);
+        for (int i = 1; i < horses.size(); i++) {
+            if (horses.get(i).distance > fastest.distance) {
+                fastest = horses.get(i);
+            }
+        }
+        return fastest;
+    }
+
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().name + '!');
+    }
+
+    void run() {
+        for (int i = 1; i < 101; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    void move() {
+        for (int i = 0; i < getHorses().size(); i++) {
+            getHorses().get(i).move();
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < getHorses().size(); i++) {
+            getHorses().get(i).print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+}
---
+++ Hippodrome/Horse.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task21.task2113;
+
+public class Horse {
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String name;
+    public double speed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double distance;
+
+    public void move() {
+        distance+=speed*Math.random();
+    }
+    public void print() {
+        for (int i = 0; i < Math.round(distance-0.5); i++) {
+            System.out.print('.');
+        }
+        System.out.print(name + "\n");
+    }
+}
