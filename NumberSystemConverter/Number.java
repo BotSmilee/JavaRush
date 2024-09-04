f0332becce1ffd1ece5273d292e4e1ddc6778353
RavenSmile <94220501+BotSmilee@users.noreply.github.com>
2024-09-04 17:57:01 +0500

    Update Number.java

--- NumberSystemConverter/Number.java
+++ NumberSystemConverter/Number.java
@@ -7,13 +7,13 @@ public class Number {
     public Number(NumberSystem numberSystem, String digit) {
         int numberSystemIntValue = numberSystem.getNumberSystemIntValue();
         if (numberSystemIntValue == 10) {
-            if (!digit.matches("^\\d+$")) throw new NumberFormatException();
+            if (!digit.matches("^\\d+$")) throw new NumberFormatException(); //Цифры десятичной системы счисления
         } else if (numberSystemIntValue == 12) {
-            if (!digit.matches("^[^\\sc-zA-Z]+$")) throw new NumberFormatException();
+            if (!digit.matches("^[^\\sc-zA-Z]+$")) throw new NumberFormatException(); //Цифры двенадцатеричной системы счисления
         } else if (numberSystemIntValue == 16) {
-            if (!digit.matches("^[^\\sg-zA-Z]+$")) throw new NumberFormatException();
+            if (!digit.matches("^[^\\sg-zA-Z]+$")) throw new NumberFormatException(); //Цифры десятичной системы счисления
         } else if (numberSystemIntValue > 1 && numberSystemIntValue < 10)
-            if (!digit.matches("^[0-" + (numberSystemIntValue-1) + "\\d]+$")) throw new NumberFormatException();
+            if (!digit.matches("^[0-" + (numberSystemIntValue-1) + "\\d]+$")) throw new NumberFormatException(); //Цифры других систем счисления
         this.numberSystem = numberSystem;
         this.digit = digit;
     }
